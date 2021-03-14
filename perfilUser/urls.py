47e9cd4e1a1e9c8ab23d545a33c4a3da97b21b53
[fabi] <[nestorfabi678@gmail.com]>
2021-03-13 22:07:07 -0300

    intento fallido de login

--- perfilUser/urls.py
+++ perfilUser/urls.py
@@ -5,5 +5,6 @@ app_name = "perfilUser"
 urlpatterns = [
     path("perfil_user/", views.perfil_user, name="Usuario"),
     path("iniciar/", views.iniciar_s, name="iniciar"),
-    path("cerrar/", views.cerrar_s, name="cerrar")
+    path("cerrar/", views.cerrar_s, name="cerrar"),
+    path("register/", views.register, name="register"),
 ]
